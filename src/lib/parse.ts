4c5ca2a8cb0004f8b5365a042c03a8ba4e581208
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-11-05 08:39:49 -0700

    Display parsed header on page

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -76,6 +76,28 @@ export function parseCacheControl(header: string): CacheControl {
 	return result;
 }
 
+export function secondsToString(seconds: number) {
+	// adapted from https://stackoverflow.com/a/34270811/14808988
+	var levels: [number, string][] = [
+		[Math.floor(seconds / 31536000), 'years'],
+		[Math.floor((seconds % 31536000) / 86400), 'days'],
+		[Math.floor(((seconds % 31536000) % 86400) / 3600), 'hours'],
+		[Math.floor((((seconds % 31536000) % 86400) % 3600) / 60), 'minutes'],
+		[(((seconds % 31536000) % 86400) % 3600) % 60, 'seconds']
+	];
+	var returntext = '';
+
+	for (var i = 0, max = levels.length; i < max; i++) {
+		if (levels[i][0] === 0) continue;
+		returntext +=
+			' ' +
+			levels[i][0] +
+			' ' +
+			(levels[i][0] === 1 ? levels[i][1].substring(0, levels[i][1].length - 1) : levels[i][1]);
+	}
+	return returntext.trim();
+}
+
 function parseNumber(s: string) {
 	return parseInt(s);
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { afterNavigate, goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { parseCacheControl, secondsToString } from '$lib/parse';
+
+	$: cacheControl = $page.url.searchParams.get('cache') ?? '';
+	$: parsed = parseCacheControl(selectedExample || cacheControl);
+
+	afterNavigate(() => {
+		selectedExample = '';
+	});
+
+	function enhance(e: SubmitEvent) {
+		const data = new FormData(e.target as HTMLFormElement);
+		// https://github.com/microsoft/TypeScript/issues/30584
+		const params = new URLSearchParams(data as any).toString();
+		goto(`?${params.toString()}`, { keepFocus: true });
+
+		e.preventDefault();
+	}
+
+	function display(value: any) {
+		if (typeof value === 'number') {
+			return toDurationString(value);
+		} else if (typeof value === 'boolean') {
+			return value ? 'yes' : 'no';
+		}
+		return value;
+	}
+
+	function toDurationString(seconds: number) {
+		return secondsToString(seconds);
+	}
+
+	let examples = [
+		'max-age=1245',
+		's-maxage=604873',
+		'public, max-age=604800, immutable',
+		'no-store'
+	];
+
+	let selectedExample = '';
+</script>
+
+<form on:submit={enhance}>
+	<label for="cache">Cache-Control: </label>
+	<input id="cache" value={cacheControl} name="cache" />
+	<button>Explain</button>
+</form>
+
+<p>Or use one of these examples:</p>
+{#each examples as example, idx}
+	<div>
+		<input id="example-{idx}" type="radio" value={example} bind:group={selectedExample} />
+		<label for="example-{idx}">{example}</label>
+	</div>
+{/each}
+
+<ul>
+	{#each Object.entries(parsed) as [k, v]}
+		<li>{k}: {display(v)}</li>
+	{/each}
+</ul>
